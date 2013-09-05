6189d4da06ee27c110e424d51506459ebe662599
Gaganpreet <gaganpreet.arora@gmail.com>
2013-09-05 14:48:07 +0530

    Use FileIO instead of passing the offset around

--- src/reader.py
+++ src/reader.py
@@ -6,6 +6,10 @@ class InvalidKWallet(Exception): pass
 class PasswordMissing(Exception): pass
 
 class KWalletReader:
+    '''
+        Class to read a kwallet file, verify it's valid, 
+        use password to decrypt and keep the encrypted part
+    '''
     def __init__(self, filename, password=None):
         ''' Initialize a KWallet.
 
@@ -13,9 +17,8 @@ class KWalletReader:
                 filename: Path to KWallet file
                 password: Password for the wallet
         '''
-        with open(filename, "rb") as f:
-            self.contents = f.read()
-            self.encrypted = self.parse_and_verify_kwallet(self.contents)
+        with open(filename, "rb") as kwallet_file:
+            self.encrypted = self.parse_and_verify_kwallet(kwallet_file)
 
             self.key = None
             if password:
@@ -36,10 +39,9 @@ class KWalletReader:
         if not self.key:
             raise PasswordMissing('Set the password first')
 
-            
         # Unfortunately, KWallet's Blowfish implementation is a little wrong
-        # It seems to have the wrong endianness than of native systems, this following
-        # line fixes that
+        # It seems to have the wrong endianness than of native systems, 
+        # this following line fixes that
         self.encrypted = util.switch_endianness(self.encrypted)
 
         bf = Blowfish.new(self.key)
@@ -48,64 +50,59 @@ class KWalletReader:
         # And switch the endianness again
         return util.switch_endianness(decrypted)
 
-    def parse_and_verify_kwallet(self, contents):
+    def parse_and_verify_kwallet(self, kwallet_file):
         ''' Open a kwallet file and do some sanity checks:
                 1 Verify magic header
                 2 Verify kwallet compatibility
                 3 Do some for loops I have no idea about
                 4 Get the actual encrypted text, verify it's divisible by block_size and return it
         '''
-        offset = 0
+        self._verify_magic(kwallet_file)
+        self._verify_compatibility(kwallet_file)
+        self._weird_loop(kwallet_file)
 
-        # This might have been better handled with StringIO
-        offset = self._verify_magic(offset)
-        offset = self._verify_compatibility(offset)
-        offset = self._weird_loop(offset)
-        self._verify_length(offset)
-        return self.contents[offset:]
+        remaining = kwallet_file.read()
+        if self._verify_length(remaining) == True:
+            return remaining
     
-    def _verify_magic(self, offset):
+    @staticmethod
+    def _verify_magic(kwallet_file):
         ''' Verify magic header of kwallet file '''
         kwallet_magic = b'KWALLET\n\r\0\r\n'
         magic_len = len(kwallet_magic)
-        if self.contents[:magic_len] != kwallet_magic:
+        if kwallet_file.read(magic_len) != kwallet_magic:
             raise InvalidKWallet('Magic header doesn\'t match')
-        offset += magic_len
-        return offset
 
-    def _verify_compatibility(self, offset):
+    @staticmethod
+    def _verify_compatibility(kwallet_file):
         ''' Verify kwallet compatibility
-            # Details of kwallet_info
-            # First byte - major version - should be 0
-            # Second byte - minor version - should be 0
-            # Third byte - cipher used - CBC == 0
-            # Fourth byte - hash used - SHA1 == 0
+            Details of kwallet_info:
+                # First byte - major version - should be 0
+                # Second byte - minor version - should be 0
+                # Third byte - cipher used - CBC == 0
+                # Fourth byte - hash used - SHA1 == 0
         '''
-        kwallet_info = self.contents[offset:offset+4]
-        if kwallet_info != b'\0'*4:
+        if kwallet_file.read(4) != b'\0'*4:
             raise InvalidKWallet('Filetype not supported')
-        offset += 4
-        return offset
 
-    def _weird_loop(self, offset):
+    @staticmethod
+    def _weird_loop(kwallet_file):
         ''' 
             I have no idea wtf this block of code does. 
             It's a copy from C++ code to Python - the original
             C++ code is not commented
         '''
-        hash_count = util.to_int(self.contents[offset:offset+4])
-        offset += 4
-        for i in range(hash_count):
-            offset += 16
-            fsz = util.to_int(self.contents[offset:offset+4])
-            offset += 4
-            for j in range(fsz):
-                offset += 16
-        return offset
-
+        hash_count = util.to_int(kwallet_file.read(4))
+        for _ in range(hash_count):
+            kwallet_file.read(16)
+            fsz = util.to_int(kwallet_file.read(4))
+            kwallet_file.read(16 * fsz)
           
-    def _verify_length(self, offset):
-        ''' Verify that the length of encrypted content is divisible by block size (8) '''
-        enc_length = len(self.contents) - offset
-        if enc_length % 8:
+    @staticmethod
+    def _verify_length(encrypted):
+        ''' Verify that the length of encrypted content is 
+            divisible by block size (8) 
+        '''
+        if len(encrypted) % 8:
             raise InvalidKWallet("File contents are not valid")
+        return True
