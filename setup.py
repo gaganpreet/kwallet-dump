769e3df21c5c946fcc833adf2cab2f8a73707713
Gaganpreet <gaganpreet.arora@gmail.com>
2013-09-15 20:59:43 +0530

    Added CHANGES.txt, bumped up version
    * Also convert Markdown to reST using pandoc for long description before
    * upload

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
+import subprocess
 from distutils.core import setup
 
 requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
+
+description = 'Access KWallet files without KDE desktop'
+try:
+    subprocess.call(["pandoc", "README.md", "-f", "markdown", "-t", "rst", "-o", "README.rst"])
+    long_description = open("README.rst").read()
+except OSError:
+    print("Pandoc not installed")
+    long_description = description
 
 classifiers = ['Environment :: Console',
                'Programming Language :: Python :: 2',
@@ -14,16 +23,18 @@ classifiers = ['Environment :: Console',
                'Topic :: Security :: Cryptography'
                ]
 
+version = open('CHANGES.txt').readlines()[0][1:].strip()
+
 setup(name='kwallet-dump',
-      version='1.0.0',
-      description='Access KWallet files without KDE desktop',
+      version=version,
+      description=description,
       author='Gaganpreet Singh Arora',
       author_email='gaganpreet.arora@gmail.com',
       url='https://github.com/gaganpreet/kwallet-dump',
       scripts=['src/kwallet-dump.py',],
       keywords=['KWallet', 'KDE', 'kwl'],
       install_requires=requirements,
-      long_description=open('README.md').read(),
+      long_description=long_description,
       packages=['kwallet_dump'],
       package_dir = {'kwallet_dump': 'src/kwallet_dump'},
       classifiers=classifiers
